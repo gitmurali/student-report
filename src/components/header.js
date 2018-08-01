df153a7cb3ea9d934694df7c89014a1ef3c0be53
KTCORP\Murali.Nutalapati <murali.nutalapati@katerra.com>
2018-08-01 12:39:32 +0530

    nav styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,30 @@
 import React, {Component} from 'react';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {Link} from "react-router";
+import {Nav, Navbar, NavItem} from "react-bootstrap";
 
 export default class Header extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
     return (
       <div>
-        <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="/">Students</a>
-            </div>
-            <ul className="nav navbar-nav">
-              <li className="active">
-                <Link
-                  to="/"
-                >Home</Link>
-              </li>
-              <li className="">
-                <Link
-                  to="/create"
-                >Create Student</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Students</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="/">
+              Home
+            </NavItem>
+            <NavItem eventKey={2} href="/create">
+              Create Student
+            </NavItem>
+          </Nav>
+        </Navbar>;
       </div>
     );
   }
